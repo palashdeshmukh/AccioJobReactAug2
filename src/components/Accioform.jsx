436c01d4c7d86b2df0ca3a35bdcb7415379469d9
palashdeshmukh <palash.deshmukh02@gmail.com>
2023-08-19 11:13:38 -0400

    Update package-lock.json and package.json2

--- src/components/Accioform.jsx
+++ src/components/Accioform.jsx
@@ -5,7 +5,7 @@ function Accioform() {
     <div className='w-100 d-flex border justify-content-center'>
     <div className='w-50 d-flex flex-column align-items-center border p-5'>
     <form className='w-50  p-4 rounded-4  border'>
-    <caption className="w-100 d-block">Welcome back!<img height="30px" src={hi}/></caption>
+    <caption className="w-100 d-block">Welcome back!<img height="30px" src={hi} alt="hi"/></caption>
     <h3 className="fw-bold pt-0 pb-2">Sign in to your account</h3>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label ">Email address</label>
